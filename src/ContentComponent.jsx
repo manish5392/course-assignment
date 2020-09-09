8c1432b8c48926107ffe29964942032032ddcd83
Manish <manish.kumar@aline-consulting.com>
2020-09-09 20:47:35 +0530

    course assignment

--- src/ContentComponent.jsx
+++ src/ContentComponent.jsx
@@ -1,55 +1,75 @@
 import React, { Component } from 'react';
+import data from './data';
 import './style.css';
 
 export default class ContentComponent extends Component {
 
-    state = {
-        showStreaming: false,
-        showSetting: false,
-    }
-
     render() {
-        const { showStreaming, showSetting } = this.state;
+        console.log(data);
 
         return (
             <div className="App">
-                {
-                    !showStreaming &&
-                    <div className="showVideo" onClick={this.showVideo}>Show Video</div>
-                }
-                {
-                    showStreaming &&
-                    <div>
-                        <div className="header fas fa-cog" onClick={this.showSetting} title="Settings">
-                            {
-                                showSetting &&
-                                <div className="setting-content">
-                                    <div className="setting-close fas fa-times" onClick={(e) => this.hideSetting(e)} title="Close Settings"></div>
-                                </div>
-                            }
-                        </div>
-                        <div className="footer fas fa-phone" onClick={this.hideVideo} title="Close Video"></div>
-                        <iframe className="frame" src="https://www.youtube.com/embed/YTJg8q9Q940" title="Video Stream"></iframe>
+                <div className="header">
+                    <div className="div1">
+                        <div>Crash Course</div>
+                        <div className="child-div1">IIT-JEE</div>
                     </div>
-                }
+                    <div className="div2">
+                        <div>12th Grade</div>
+                        <div className="child-div2">IIT-JEE</div>
+                    </div>
+                    <div className="div3">
+                        <div>Crash Course</div>
+                        <div className="child-div3">NEET</div>
+                    </div>
+                </div>
+                <div className="content">
+                    <div className="timings">
+                        {
+                            data.timeSlots.map((times, index) => {
+                                return (
+                                    <div key={index} className="each-time">
+                                        {times}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="courses">
+                        {
+                            data.courses.map((eachCourse, index) => {
+                                return (
+                                    <div key={index} className="course-container">
+                                        {
+                                            eachCourse.details.map((eachDetails, ind) => {
+                                                const courseClassName = eachDetails.courseName === 'Physics' ? 'each-course physics' :
+                                                    (eachDetails.courseName === 'Chemistry' ? 'each-course chemistry' : 'each-course mathematics');
+
+                                                const detailsClass = eachDetails.courseName === 'Physics' ? 'course-details physics-details' :
+                                                    (eachDetails.courseName === 'Chemistry' ? 'course-details chem-details' : 'course-details maths-details')
+                                                return (
+                                                    <div key={ind} className={courseClassName}>
+                                                        <div className={detailsClass}>{eachDetails.courseName}</div>
+                                                        <div className='course-details'>{eachDetails.topicName}</div>
+                                                        {
+                                                            eachDetails.teacherName &&
+                                                            <div className='course-details'>By<span className="teacher">{eachDetails.teacherName}</span></div>
+                                                        }
+                                                        <div className='course-details'>{eachDetails.timings}</div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className="live">Live</div>
+                <div className="now-playing">Now Playing</div>
+                <div className="footer" />
             </div>
         )
-    }
-
-    showVideo = () => {
-        this.setState({ showStreaming: true });
-    }
-
-    hideVideo = () => {
-        this.setState({ showStreaming: false });
-    }
-
-    showSetting = () => {
-        this.setState({ showSetting: true });
-    }
-
-    hideSetting = e => {
-        e.stopPropagation();
-        this.setState({ showSetting: false });
     }
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    timeSlots: ['08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM'],
+    courses2: [
+        { courseName: 'Chemistry', topicName: 'Gaseous state - 1', timings: '08:00 - 09:25', teacherName: 'Vijay Kishore'},
+        { courseName: 'Chemistry', topicName: 'Gaseous state - 1', timings: '08:00 - 09:25', teacherName: 'Vijay Kishore'},
+        { courseName: 'Chemistry', topicName: 'Gaseous state - 1', timings: '08:00 - 09:25', teacherName: 'Vijay Kishore'},
+        { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Mathematics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Mathematics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Mathematics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+        ],
+    courses: [
+        {
+            courseName: 'Chemistry',
+            details: [
+                { courseName: 'Chemistry', topicName: 'Gaseous state - 1', timings: '08:00 - 09:25', teacherName: 'Vijay Kishore'},
+                { courseName: 'Chemistry', topicName: 'Gaseous state - 1', timings: '08:00 - 09:25', teacherName: 'Vijay Kishore'},
+                { courseName: 'Chemistry', topicName: 'Gaseous state - 1', timings: '08:00 - 09:25', teacherName: 'Vijay Kishore'},
+            ]
+        },
+        {
+            courseName: 'Physics',
+            details: [
+                { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+                { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+                { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+            ]
+        },
+        {
+            courseName: 'Mathematics',
+            details: [
+                { courseName: 'Mathematics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+                { courseName: 'Mathematics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+                { courseName: 'Mathematics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+            ]
+        },
+        {
+            courseName: 'Physics',
+            details: [
+                { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+                { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+                { courseName: 'Physics', topicName: 'Previous jee main questions discussion', timings: '', teacherName: ''},
+            ]
+        }
+    ],
+};
+
+export default data;
